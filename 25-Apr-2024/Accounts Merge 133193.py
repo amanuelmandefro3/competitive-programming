3d55484f48f065b95fe918328f7c3fc8dff28606
theamano <106990287+amanuelmandefro3@users.noreply.github.com>
2024-04-25 15:38:55 +0300

    Created 'Accounts Merge' by A2SV Hub

---
+++ 25-Apr-2024/Accounts Merge 133193.py
@@ -0,0 +1,57 @@
+# Problem: Accounts Merge - https://leetcode.com/problems/accounts-merge/
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        parent, n= {}, len(accounts)
+        ans = []
+        pos = {}
+        ans_val = [set() for _ in range(n)]
+        def find(member):
+            root = member
+            while root != parent[root]:
+                root = parent[root]
+            while member != root:
+                p = parent[member]
+                parent[member] = root
+                member = p
+            return root  
+
+        def union(x, y):
+            px = find(x)
+            py = find(y)
+            if px != py:
+                if px < py:
+                    parent[py] = px
+                else:
+                    parent[px] = py
+
+
+        for i in range(n):
+            ans.append(accounts[i][0])
+            for j in range(1, len(accounts[i])):
+                if accounts[i][j] not in parent:
+                    parent[accounts[i][j]] = accounts[i][j]
+                    pos[accounts[i][j]] = i
+            for j in range(1, len(accounts[i])-1):
+                union(accounts[i][j], accounts[i][j+1])
+
+        for i in range(n):
+            for j in range(1, len(accounts[i])):
+                px = find(accounts[i][j])
+                idx = pos[px]
+                ans_val[idx].add(accounts[i][j])     
+        
+
+        res = []
+        for i in range(n):
+            if len( ans_val[i]) > 0:    
+                account = [ans[i]]
+                emails = list(ans_val[i])
+                emails.sort()
+                account.extend(emails)
+                res.append(account)
+           
+
+
+        return res              
+    
