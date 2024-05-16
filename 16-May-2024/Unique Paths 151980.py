a51a0f307edb3aa181f2b016f1952e1ac2f99dad
theamano <106990287+amanuelmandefro3@users.noreply.github.com>
2024-05-16 14:35:35 +0300

    Created 'Unique Paths' by A2SV Hub

---
+++ 16-May-2024/Unique Paths 151980.py
@@ -0,0 +1,17 @@
+# Problem: Unique Paths - https://leetcode.com/problems/unique-paths/
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        memo = {}
+        def dp(i,j):
+            if i == m-1 and j == n-1:
+                return 1
+            if i >= m or j >= n:
+                return 0
+            if (i,j) in memo:
+                return memo[(i,j)]
+            else:
+                memo[(i,j)] = dp(i+1, j) + dp(i,j+1)    
+            return memo[(i,j)]    
+                       
+        return dp(0,0)          
