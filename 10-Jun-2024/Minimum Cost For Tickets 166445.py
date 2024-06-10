61f465c639ef1a03381b2764a054c51b36beb2d3
theamano <106990287+amanuelmandefro3@users.noreply.github.com>
2024-06-10 20:28:59 +0300

    Created 'Minimum Cost For Tickets' by A2SV Hub

---
+++ 10-Jun-2024/Minimum Cost For Tickets 166445.py
@@ -0,0 +1,23 @@
+# Problem: Minimum Cost For Tickets - https://leetcode.com/problems/minimum-cost-for-tickets/
+
+class Solution:
+    def mincostTickets(self, days: List[int], costs: List[int]) -> int:
+        memo = {}
+        def dp(i):
+            if i >= len(days):
+                return 0
+            #memo has current state? return    
+            if i in memo:
+                return memo[i] 
+
+            one = costs[0] + dp(bisect_right(days, days[i]))
+            seven = costs[1] + dp(bisect_right(days, days[i]+6))
+            thirty = costs[2] + dp(bisect_right(days, days[i]+29))          
+            memo[i] = min(one, seven, thirty)
+ 
+
+            return memo[i] 
+
+        return dp(0)           
+
+                  
