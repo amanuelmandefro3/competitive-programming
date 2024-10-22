fbf9b78f89409657a5dcaed6338671a0931cc93e
theamano <106990287+amanuelmandefro3@users.noreply.github.com>
2024-10-22 06:49:47 +0300

    Created 'Find Indices With Index and Value Difference I' by A2SV Hub

---
+++ 22-Oct-2024/Find Indices With Index and Value Difference I 221302.py
@@ -0,0 +1,10 @@
+# Problem: Find Indices With Index and Value Difference I - https://leetcode.com/problems/find-indices-with-index-and-value-difference-i/description/
+
+class Solution:
+    def findIndices(self, nums: List[int], indexDifference: int, valueDifference: int) -> List[int]:
+        
+        for i in range(len(nums)):
+            for j in range(i, len(nums)):
+                if abs(i-j) >= indexDifference and abs(nums[j]-nums[i]) >= valueDifference:
+                    return [i,j]
+        return [-1,-1]             
